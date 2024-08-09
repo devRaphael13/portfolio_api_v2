692d9ae3d414991c77d676d98ce73823248224cc
Raphael <raphael.ezeigwe@yahoo.com>
2024-08-09 01:51:49 +0100

    Feat: add basic viewsets

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Technology, Experience, Project
+from .serializers import TechSerializer, ExpSerializer, ProjSerializer
 
-# Create your views here.
+class TechViewSet(ModelViewSet):
+    queryset = Technology.objects.all()
+    serializer_class = TechSerializer
+
+class ExpViewSet(ModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExpSerializer
+    
+class ProjViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjSerializer
+
