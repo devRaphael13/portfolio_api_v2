87b05ec31c6bedf764c80585f0ef6992f02f2433
Raphael <raphael.ezeigwe@yahoo.com>
2024-08-09 01:39:09 +0100

    Feat: add database models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Technology(models.Model):
+    name = models.CharField(max_length=120)
+
+class Experience(models.Model):
+    company = models.CharField(max_length=120)
+    position = models.CharField(max_length=120)
+    description = models.TextField()
+    tech_used = models.ManyToManyField(Technology, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Project(models.Model):
+    name = models.CharField(max_length=120)
+    description = models.TextField()
+    proj_type = models.CharField(max_length=20, choices=[["p", "Personal"], ["w", "Work"]])
+    employer = models.ForeignKey(Experience, blank=True, null=True)
+    link = models.URLField()
+    tech_used = models.ManyToManyField(Technology, blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+
